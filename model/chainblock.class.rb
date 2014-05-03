2cdfb9a4b793ea2d3c4960d40185d2c13e4859ea
icanprogram <icanprogramfedoracoins@gmail.com>
2014-05-03 19:29:39 +0000

    Added difficulty calculation, time and transactions

--- model/chainblock.class.rb
+++ model/chainblock.class.rb
@@ -3,6 +3,24 @@ module Aura
 		# Represents a block in a particular chain
 		class ChainBlock < ActiveRecord::Base
 			self.table_name = 'chain_summary'
+			
+			# Blatantly copied from bitcoin-abe and translated to Ruby
+			def difficulty
+				shift = 8 * (((block_nBits >> 24) & 0xff) - 3)
+				bits = block_nBits & 0x7fffff
+				sign = block_nBits & 0x800000 ? 1 : -1
+				target = sign * (shift >= 0 ? bits << shift : bits >> -shift)
+				return ((1 << 224) - 1) * 1000 / (target + 1) / 1000.0
+			end
+			
+			#Returns ISO 8601 formatted timestamp for this block
+			def time
+				DateTime.strptime(block_nTime.to_s, "%s").to_s
+			end
+			
+			def transactions
+				[]
+			end
 		end
 	end
 end
